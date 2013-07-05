47c4c20a8b5a8fd593a585c024ef8f8b2bd9eab3
Rodolfo Federico Gamarra <rgamarra@gmail.com>
2013-07-05 00:58:21 -0300

    text from html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require("fs");
 
 var app = express.createServer(express.logger());
 
+var buff = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buff.toString());
 });
 
 var port = process.env.PORT || 5000;
